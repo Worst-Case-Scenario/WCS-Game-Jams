704a318a0e1e77d93f2d9ebc4f74a52ead45d1b5
elderaka <laudadraka@gmail.com>
2023-07-21 03:01:50 +0700

    Splash + Animation

--- Scripts/Splashscreen.gd
+++ Scripts/Splashscreen.gd
@@ -1,14 +1,15 @@
 extends VBoxContainer
 
-var fadein = true
+var fadein = false
 var fadeout = false
 var next_room = false
-
+var timer = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$disclaimer.modulate.a = -2
 	$logo.modulate.a = -2
+	fadein = true
 
 var i = 0
 
@@ -17,8 +18,11 @@ func _process(delta):
 	#changing the text for splash screen
 	if i == 0:
 		if fadein:
-			$logo.modulate.a += 3*delta
-		if $logo.modulate.a >= 4:
+			if $logo.modulate.a < 1:
+				$logo.modulate.a += 3*delta
+			if $logo.modulate.a >= 1:
+				timer += delta
+		if timer > 2:
 			fadein = false
 			fadeout = true
 		if fadeout:
@@ -26,11 +30,15 @@ func _process(delta):
 		if $logo.modulate.a <= -1 and fadeout:
 			fadeout = false
 			fadein = true
+			timer = 0
 			i += 1
 	elif i == 1:
 		if fadein:
-			$disclaimer.modulate.a += 3*delta
-		if $disclaimer.modulate.a >= 4:
+			if $disclaimer.modulate.a < 1:
+				$disclaimer.modulate.a += 3*delta
+			if $disclaimer.modulate.a >= 1:
+				timer += delta
+		if timer > 2:
 			fadein = false
 			fadeout = true
 		if fadeout:
@@ -40,4 +48,4 @@ func _process(delta):
 			fadein = true
 			i += 1
 	else:
-		get_tree().change_scene_to_file("res://main.tscn")
+		get_tree().change_scene_to_file("res://Scene/title.tscn")
---
+++ Scripts/TitleScreen.gd
@@ -0,0 +1,55 @@
+extends Control
+
+var pos = 0
+var menu_length = 3
+var fadeOut = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("ping")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var down = int(Input.is_action_just_pressed("down"))
+	var up = int(Input.is_action_just_pressed("up"))
+	print(int(Input.is_action_just_pressed("up")))
+	if not fadeOut:
+		pos += down - up
+	if pos >= menu_length:
+		pos = 0
+	if pos < 0:
+		pos = menu_length - 1
+
+	match pos:
+		0:	
+			#Gunanya untuk mengganti warna apabila posisi berada di angka tersebut
+			$Mulai.set("theme_override_colors/font_color", Color(1, 0, 0))
+			$Keluar.set("theme_override_colors/font_color", Color(1,1,1))
+			$Pengaturan.set("theme_override_colors/font_color", Color(1,1,1))
+
+		1:
+			$Mulai.set("theme_override_colors/font_color", Color(1,1,1))
+			$Pengaturan.set("theme_override_colors/font_color", Color(1, 0, 0))
+			$Keluar.set("theme_override_colors/font_color", Color(1,1,1))
+		2:
+			$Mulai.set("theme_override_colors/font_color", Color(1,1,1))
+			$Keluar.set("theme_override_colors/font_color", Color(1, 0, 0))
+			$Pengaturan.set("theme_override_colors/font_color", Color(1,1,1))
+
+	if Input.is_action_just_pressed("accept"):
+		fadeOut = true
+	if fadeOut:
+		modulate.a -= 2*delta
+	if modulate.a <= -1:
+		match pos:
+			0: 
+				get_tree().change_scene_to_file("res://main.tscn")
+			1:
+				pass
+			2:
+				get_tree().quit()
+
+	pass
+
