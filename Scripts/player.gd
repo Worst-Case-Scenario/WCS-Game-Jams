fa32caab9d087c4251e69f1346e8d89120fa30c4
Ditrogen <yohan.loadout@gmail.com>
2023-07-22 22:29:37 +0700

    animation+bg
    player animation fixed, tilemaps, parallax bg

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,14 +4,23 @@ extends CharacterBody2D
 @export var gravity = 30
 @export var jump_force = 300
 
+const max_jump = 2
+var current_jump = 0
+
 @onready var animSprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	if !is_on_floor() && velocity.y < 1000:
-		velocity.y += gravity
+	if !is_on_floor():
+		if velocity.y < 1000:
+			velocity.y += gravity
+	else:
+		current_jump = 0
 	
 	if Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_force
+		if current_jump < max_jump:
+			velocity.y = -jump_force
+			current_jump += 1
+		
 	
 	var horizontal_direction = Input.get_axis("move_left","move_right")
 	velocity.x = speed * horizontal_direction
@@ -19,12 +28,12 @@ func _physics_process(delta):
 	
 	if velocity.y < 0:
 		animSprite.play("jump")
-	elif velocity.x != 0:
+	elif velocity.x < 0:
+		animSprite.flip_h = true
+		animSprite.play("moving")
+	elif velocity.x > 0:
+		animSprite.flip_h = false
 		animSprite.play("moving")
 	else:
 		animSprite.play("idle")
 	
-	update_rotation(velocity)
-
-func update_rotation(direction):
-	animSprite.flip_h = direction.x < 0
